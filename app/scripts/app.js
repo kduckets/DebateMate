28c85c355721c516700ca143cea9e77a03ea209d
kduckets <kmditroia@gmail.com>
2015-06-17 16:39:13 -0700

    Added user profile

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,6 +50,10 @@ var app = angular
     }
   }
 })
+    .when('/users/:userId', {
+  templateUrl: 'views/profile.html',
+  controller: 'ProfileCtrl'
+})
       .otherwise({
         redirectTo: '/'
       });
